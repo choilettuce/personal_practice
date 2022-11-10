e5199f10f9d272360e8f681784adbc6e734ca706
choilettuce <tkdgnstkdcn@naver.com>
2022-11-10 21:45:11 +0900

    feat: array object

---
+++ JS/DataType.js
@@ -0,0 +1,24 @@
+//number type
+let n1 = 1234;
+let n2 = 2.123;
+
+//string type
+let s1 = "hello";
+let s2 = 'world';
+
+//boolean type
+let b1 = true;
+let b2 = false;
+
+//null type
+let n = null;
+
+// undefined type
+let u1;
+let u2 = undefined;
+
+// Array type
+let arr = [1,2,3,4]
+
+//Object type
+let obj = {a : "apple" , b : "banana", c: "carrot"}
---
+++ JS/array.js
@@ -0,0 +1,19 @@
+//number 타입으로만 이루어진 배열
+let arr1 = [1, 2, 3, 4, 5];
+
+//string 타입으로만 이루어진 배열
+let arr2 = ["h","e", "l", "l", "o"];
+
+//number 타입과 string 타입을 함께 사용한 배열
+let arr3 = [1, "h", 2, "e"];
+
+//다양한 자료형 함께 사용한 배열 
+let arr4 = [true, 1, undefined, false, "h", 2, null];
+
+console.log(arr1[0]);
+
+console.log(arr2[1]);
+
+console.log(arr3[2]);
+
+console.log(arr4[3]);
