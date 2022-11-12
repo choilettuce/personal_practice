fae77198fd39b2c288c27e33414474c256570e61
choilettuce <tkdgnstkdcn@naver.com>
2022-11-12 20:56:14 +0900

    study : obj

--- JS/Obj.js
+++ JS/Obj.js
@@ -1,4 +1,28 @@
-//객체
+//값으로 String 타입만을 사용한 객체
+let obj1 = {a : "apple", b : "banana", c: "carrot"};
+
+// 값으로 number 타입만을 사용한 객체
+let obj2 = {a : 1, b: 2, c:3};
+
+// 값으로 다양한 자료형들을 함께 사용한 객체
+let obj3 = {a : "hello", b : 100, c: [1,2,3,4]}
+
+// 값으로 객체를 사용한 객체
+let obj4 = {
+    a: {a1 :1, a2:2},
+    b: {b1 :3, b2:4},
+    c: {c1 :5, c2:6},
+}
+
+// 출력
+console.log(obj1['a']);
+console.log(obj2.a);
+console.log(obj3['c']);
+console.log(obj4.c)
+console.log(obj4.c.c2);
+
+
+// 함수를 값으로 할수도 있다.
 var cat = {
     name: "나비",
     age : 1,
@@ -10,50 +34,23 @@ console.log(cat);
 console.log(cat.name);
 console.log(cat.mew());
 
-var me = {
+
+// 객체에 추가 하는 방법
+let lettuce = {
     name : 'SH' ,
     birth : '0102'
 
 };
 
-me['gender'] = 'M';
+lettuce['gender'] = 'M';
 
-console.log(me['gender']);
+console.log(lettuce['gender']);
 
+let obj = {} // 빈 객체 선언
 
-//배열
-var arr1 = [12, 15, 18, 24, 29];
-console.log(arr1.length);
-arr1.push("91");
-console.log(arr1);
-arr1.pop();
-console.log(arr1);
-arr1.unshift(61);
-console.log(arr1);
-arr1.shift();
-console.log(arr1);
-
-console.log(arr1.indexOf(18));
+// 1. dot notation 표기법
+obj.name = 'lettuce';
+obj.age = 20
+obj.address = 'Inchoen'
 
 
-
-
-//참조자료형 
-var a1 = [1, 2, 3, 4, 5];
-var a2 = a1;
-console.log("a1 : ", a1);
-console.log("a2 : ", a2);
-
-a1.pop();
-console.log("a1 : ", a1);
-console.log("a2 : ", a2);
-
-//자료형확인
-let t1 = typeof('문자');
-let t2 = typeof(245);
-
-console.log(t1);
-console.log(t2);
-
-// 형변환
-Number('10');
