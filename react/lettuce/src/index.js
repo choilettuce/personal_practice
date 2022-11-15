6c7aac3c5b6c5c581e4c462b0e1f7a7d9624a22e
choilettuce <tkdgnstkdcn@naver.com>
2022-11-15 19:09:40 +0900

    study react

--- react/lettuce/src/index.js
+++ react/lettuce/src/index.js
@@ -5,11 +5,12 @@ import './index.css';
 // import Library from './chapter3/Library';
 // import Clock from './chapter04/clock';
 // import CommentList from './chapter05/CommentList';
-import NotificationList from './chapter06/NotificationList';
+// import NotificationList from './chapter06/NotificationList';
+import Counter from './Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList/>
+    <Counter/>
   </React.StrictMode>
 );
