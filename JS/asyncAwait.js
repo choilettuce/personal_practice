064abc91fb9808e4da20842cf1692147cc27a943
choilettuce <tkdgnstkdcn@naver.com>
2022-11-20 18:55:16 +0900

    study async

---
+++ JS/asyncAwait.js
@@ -0,0 +1,22 @@
+async function startAsync(age) {
+    if (age > 20) return `${age} success`;
+    else throw new Error(`${age} is not over 20`);
+  }
+  
+  const promise1 = startAsync(25);
+  promise1
+    .then((value) => {
+      console.log(value);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  
+  const promise2 = startAsync(15);
+  promise2
+    .then((value) => {
+      console.log(value);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
