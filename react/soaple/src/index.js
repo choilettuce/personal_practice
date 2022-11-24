0fc5fb963b050abde946d32bac5694e036aeb7dd
choilettuce <tkdgnstkdcn@naver.com>
2022-11-24 20:51:20 +0900

    study react chapter14

--- react/soaple/src/index.js
+++ react/soaple/src/index.js
@@ -12,11 +12,12 @@ import './index.css';
 // import AttendanceBook from './chapter10/AttendanceBook';
 // import SignUp from './chapter11/signUp';
 // import Calculator from './chapter12/Calculator';
-import ProfileCard from './chapter13/ProfileCard';
+// import ProfileCard from './chapter13/ProfileCard';
+import DarkOrLight from './chapter14/DarkOrLight';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProfileCard/>
+    <DarkOrLight/>
   </React.StrictMode>
 );  
