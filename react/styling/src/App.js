1e91dc9184989f4b6c2636b7cceabbf8e42b9b80
choilettuce <tkdgnstkdcn@naver.com>
2022-11-30 17:35:56 +0900

    study react styling

---
+++ react/styling/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+
+function App() {
+  return (
+    <>
+    <SassComponent/>
+    <CSSModule/>
+    </>
+  );
+}
+
+export default App;
