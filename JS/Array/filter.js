8c4628bbb0e52c1561624604983c1de32653af91
choilettuce <tkdgnstkdcn@naver.com>
2022-12-06 22:57:22 +0900

    foreach

---
+++ JS/Array/filter.js
@@ -0,0 +1,11 @@
+//filter()
+let words = ['banana', 'apple', 'watermelon', 'pear', 'cherry', 'strawberry'];
+let result = words.filter(x => x.length >= 6);
+
+console.log(result);
+
+
+let num = [1,2,3,4,5,6,7,8,9,10]
+let result2 = num.filter(e => e <7);
+
+console.log(result2);
---
+++ JS/Array/foreach.js
@@ -0,0 +1,7 @@
+let words = ['one', 'two', 'three', 'four']
+words.forEach(function(word) {
+  console.log(word)
+  if (word === 'two') {
+    words.shift()
+  }
+})
