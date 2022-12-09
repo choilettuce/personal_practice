e1beddef03d6380d451cf2ca9ea18c3a1491afff
choilettuce <tkdgnstkdcn@naver.com>
2022-12-09 16:47:32 +0900

    단축평가

--- JS/shortcircuitEvaluation.js
+++ JS/shortcircuitEvaluation.js
@@ -1,3 +1,11 @@
+// 단축평가 규칙
+true || anything //  => true
+false || anything // => anything
+true && anything // => anything
+false && anything // => false
+
+
+
 // 논리곱(&&) 연산자
 'a'   && 'b'   // => 'b'
 false && 'b'   // => false
@@ -7,3 +15,25 @@ false && 'b'   // => false
 'a'   || 'b'   // => 'a'
 false || 'b'   // => 'b'
 'a'   || false // => 'a'
+
+// 단축평가를 활용한 if문 대체
+let finished = true; // 주어진조건이 true
+let message = '';
+
+message = finished && '완료';
+console.log(message); // => '완료'
+
+finished = false; // 주어진 조건이 false
+
+message = finished || '미완료';
+console.log(message); // => '미완료'
+
+
+// 삼항 조건 연산자로 if ... else 문 대체
+let condition = true;
+let message2 = '';
+
+message2 =  done ? '완료' : '미완료';
+console.log(message2); // => 완료
+
+
