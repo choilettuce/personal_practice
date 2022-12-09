c31aa38b80573c6dab6c8f0ada08482e686fe45d
choilettuce <tkdgnstkdcn@naver.com>
2022-12-09 19:07:39 +0900

    study expression

---
+++ JS/expression.js
@@ -0,0 +1,27 @@
+// 리터럴 표현식
+10;
+'Hello'
+
+//식별자 표현식
+let sum;
+let person = {
+    name :  lettuce,
+}
+let arr = [1,2,3,4];
+// 선언
+
+sum;
+person.name;
+arr[1];
+
+//연산자 표현식
+1+2;
+sum = 10;
+sum !== 10;
+
+//함수 표현식
+function square() {
+    return 1
+}; // 선언
+
+square();
