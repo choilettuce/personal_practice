84213118f61a166b2ae259b3868be0911ab32dfb
choilettuce <tkdgnstkdcn@naver.com>
2022-12-17 23:59:19 +0900

    feat : movieApp

--- react/movie-app/src/index.js
+++ react/movie-app/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TodoList from './TodoList';
-import CoinTracker from './CoinTracker';
+// import TodoList from './TodoList';
+// import CoinTracker from './CoinTracker';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoList />
-    <CoinTracker />
+    {/* <TodoList />
+    <CoinTracker /> */}
+    <App/>
   </React.StrictMode>
 );
 
