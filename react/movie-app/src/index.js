a65e8eb0effd710e23a372942c2109b52cc77092
choilettuce <tkdgnstkdcn@naver.com>
2022-12-18 10:44:14 +0900

    feat movie-app

--- react/movie-app/src/index.js
+++ react/movie-app/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import TodoList from './TodoList';
 // import CoinTracker from './CoinTracker';
-import App from './App';
+import Home from './route/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <TodoList />
     <CoinTracker /> */}
-    <App/>
+    <Home/>
   </React.StrictMode>
 );
 
