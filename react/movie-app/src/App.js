025efaaf3397678d0dc967a8eb73287501e331c3
choilettuce <tkdgnstkdcn@naver.com>
2022-12-18 11:36:04 +0900

    study react route

--- react/movie-app/src/App.js
+++ react/movie-app/src/App.js
@@ -1,8 +1,15 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./route/Home";
+import Detail from "./route/Detail";
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie" element={<Detail/>} />
+        <Route path="/" element ={<Home />} />
+      </Routes>
+     </BrowserRouter>
   );
 }
 
--- react/movie-app/src/index.js
+++ react/movie-app/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import TodoList from './TodoList';
 // import CoinTracker from './CoinTracker';
-import Home from './route/Home';
-
+// import Home from './route/Home';
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <TodoList />
-    <CoinTracker /> */}
-    <Home/>
-  </React.StrictMode>
+  <App />
 );
 
