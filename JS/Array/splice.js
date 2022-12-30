f7f05dc4e3231b6e9772d29af90d0d1303d40b97
choilettuce <tkdgnstkdcn@naver.com>
2022-12-30 22:50:39 +0900

    study splice

--- JS/Array/splice.js
+++ JS/Array/splice.js
@@ -1,10 +1,10 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-const arr1 =  arr.splice(10, 2, 'a', 'b', 'c');
+// const arr1 =  arr.splice(10, 2, 'a', 'b', 'c');
 // 10번째 부터 2개의 요소 제거, 'a', 'b', 'c'인자 추가
+
+const arr1 = arr.splice(-6,4);
 console.log("arr :", arr);
 console.log("arr1 :", arr1);  
 
-const arr2 = arr.splice(-6,4);
-console.log("arr :", arr);
-console.log("arr2 : ", arr2);
+
