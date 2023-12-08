a2a7502e390d02407b8e7c8b26d02a2dcc1ecb28
choilettuce <tkdgnstkdcn@naver.com>
2023-12-09 00:20:10 +0900

    fetch practice

---
+++ JS/axios/fetch.js
@@ -0,0 +1,43 @@
+// 1. get 메소드
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
+
+// 2. POST 메소드
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    title: "Test",
+    body: "I am testing!",
+    userId: 1,
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
+// 3. PUT
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    title: "Test",
+    body: "I am testing!",
+    userId: 1,
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
+// 4. DELETE 메소드
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "DELETE",
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
