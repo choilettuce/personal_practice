e53f6938952b7eb23d1f22f8abfbcb9658b91efc
choilettuce <tkdgnstkdcn@naver.com>
2023-12-12 02:19:18 +0900

    study : cleanUp

---
+++ react/movie-app/src/components/Cleanup.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Hello() {
+    useEffect(function () {
+      console.log("hi :)");
+      return function () {
+        console.log("bye :(");
+      };
+    }, []);
+    useEffect(() => {
+      console.log("hi :)");
+      return () => console.log("bye :(");
+    }, []);
+    return <h1>Hello</h1>;
+  }
+
+
+function Cleanup() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
+    return (
+      <div>
+        {showing ? <Hello /> : null}
+        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+        </div>
+    )
+  } 
+
+export default Cleanup;
